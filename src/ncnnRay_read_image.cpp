85d7f7b02ecba2a1584da3b9fdb730a35467b154
Shlomo Kashani <shlomo@bayesian.io>
2020-10-13 18:24:08 +0300

    Must be type=ncnn::Mat::PIXEL_RGBA2RGB otherwise no face detection

--- src/ncnnRay_read_image.cpp
+++ src/ncnnRay_read_image.cpp
@@ -5,99 +5,104 @@
 #include "../bin64/_deps/raylib-src/src/external/glad.h"
 #include "../include/raygui/ricons.h"
 
+#define PL_MPEG_IMPLEMENTATION
 
-int main() {
-    SetConfigFlags(FLAG_VSYNC_HINT); // Enable V-SYNC.
+#include "../include/pl_mpeg/pl_mpeg.h"
+#include "models/neural.h"
+#include "models/LFFD.h"
 
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
-    InitWindow(screenWidth, screenHeight, "PONC");
+int main(int argc, char **argv) {
 
-    const char *mainTitle = "ncnnRay: Model Studio";
-    InitWindow(screenWidth, screenHeight, mainTitle);
-    Font gamefont = LoadFont("GameCube.ttf");
-    Sound clickSound = LoadSound("save.ogg");
-    Sound saveImageSound = LoadSound("click.ogg");
 
-    const char *torchStyle = "torch2.rgs";
-    GuiLoadStyle(torchStyle);
-    GuiFade(0.9f);
-    GuiSetStyle(DEFAULT, TEXT_SPACING, 1);
-    Color defTextCLR = GetColor(GuiGetStyle(DEFAULT, LINE_COLOR));
+    //RGB
+    Image image = LoadImage("faces.png");   // Loaded in CPU memory (RAM)
+    ncnn::Mat inmat = rayImageToNcnn(image);
+    std::cout << "Total:" << inmat.total() << std::endl;
+    std::cout << "D:" << tensorDIMS(inmat) << std::endl;;
+    Image saveImage = ncnnToRayImage(inmat);
+    ExportImage(saveImage, "faces-ncnn-rgb.png");
 
-    SetTargetFPS(60);
-    // Set all game variables.
-    // Player paddle vars.
-    int plPosX = 10;
-    int plPosY = 285;
-    // Enemy paddle vars.
-    int enPosX = 1260;
-    int enPosY = 285;
-    // Paddle shared vars.
-    int paSizX = 10;
-    int paSizY = 150;
-    // Ball vars.
-    int baPosX = 22;
-    int baPosY = 356;
-    int baVelX = 2;
+    Image imageRGBA = LoadImage("manga.png");   // Loaded in CPU memory (RAM)
+    ncnn::Mat inmatimageRGBA = rayImageToNcnn(imageRGBA);
+    std::cout << "Total:" << inmatimageRGBA.total() << std::endl;
+    std::cout << "D:" << tensorDIMS(inmatimageRGBA) << std::endl;;
+    Image saveImageimageRGBA = ncnnToRayImage(inmatimageRGBA);
+    ExportImage(saveImageimageRGBA, "manga-ncnn-rgba.png");
 
-    // Main game loop.
-    while (!WindowShouldClose()) {
-        int baVelY = GetRandomValue(-6, 7);
-        int baSizA = GetRandomValue(8, 25);
-
-        // Player paddle movement.
-        if ((IsKeyDown(KEY_UP)) && (plPosY > 5)) plPosY -= 2;
-        else if ((IsKeyDown(KEY_DOWN)) && (plPosY + 150 < 715)) plPosY += 2;
-        // Enemy paddle movement.
-        if (baPosX > 640) {
-            if ((enPosY + 70 > baPosY) && (enPosY > 5)) enPosY -= 2;
-            else if ((enPosY + 80 < baPosY) && (enPosY + 150 < 715)) enPosY += 2; }
-        // Paddle - Ball collison.
-        if ((baPosX <= plPosX + 10) && (baPosY >= plPosY) && (baPosY <= plPosY + paSizY)) baVelX = -baVelX;
-        else if ((baPosX >= enPosX - 10) && (baPosY >= enPosY) && (baPosY <= enPosY + paSizY)) {
-            baVelX = -baVelX;
-            baVelY = GetRandomValue(-2, 2); }
-        // Edge - Ball collsion.
-        if (baPosY < 5) baVelY = -baVelY;
-        else if (baPosY + 8 > 715) baVelY = -baVelY;
-        // Ball movement and Game Over logic.
-        if ((baPosX > 10) && (baPosX < 1270)) {
-            baPosX += baVelX;
-            baPosY += baVelY;
-        } else { break; }
-        // Draw to screen.
-        BeginDrawing();
-        ClearBackground(DARKGRAY);
-        // Player paddle
-        DrawRectangle(plPosX, plPosY, paSizX, paSizY, RAYWHITE);
-        // Enemy paddle.
-        DrawRectangle(enPosX, enPosY, paSizX, paSizY, DARKGREEN);
-        // Ball.
-        DrawRectangle(baPosX, baPosY, baSizA, baSizA, RED);
-        EndDrawing();
-    }
+    //RGBA
+    ncnn::destroy_gpu_instance();
+    return 0;
 }
 
-//int main(int argc, char **argv) {
+//int main() {
+//    SetConfigFlags(FLAG_VSYNC_HINT); // Enable V-SYNC.
 //
+//    const int screenWidth = 1280;
+//    const int screenHeight = 720;
+//    InitWindow(screenWidth, screenHeight, "PONC");
 //
-//    //RGB
-//    Image image = LoadImage("faces.png");   // Loaded in CPU memory (RAM)
-//    ncnn::Mat inmat = rayImageToNcnn(image);
-//    std::cout << "Total:" << inmat.total() << std::endl;
-//    std::cout << "D:" << tensorDIMS(inmat) << std::endl;;
-//    Image saveImage = ncnnToRayImage(inmat);
-//    ExportImage(saveImage, "faces-ncnn-rgb.png");
+//    const char *mainTitle = "ncnnRay: Model Studio";
+//    InitWindow(screenWidth, screenHeight, mainTitle);
+//    Font gamefont = LoadFont("GameCube.ttf");
+//    Sound clickSound = LoadSound("save.ogg");
+//    Sound saveImageSound = LoadSound("click.ogg");
 //
-//    Image imageRGBA = LoadImage("manga.png");   // Loaded in CPU memory (RAM)
-//    ncnn::Mat inmatimageRGBA = rayImageToNcnn(imageRGBA);
-//    std::cout << "Total:" << inmatimageRGBA.total() << std::endl;
-//    std::cout << "D:" << tensorDIMS(inmatimageRGBA) << std::endl;;
-//    Image saveImageimageRGBA = ncnnToRayImage(inmatimageRGBA);
-//    ExportImage(saveImageimageRGBA, "manga-ncnn-rgba.png");
+//    const char *torchStyle = "torch2.rgs";
+//    GuiLoadStyle(torchStyle);
+//    GuiFade(0.9f);
+//    GuiSetStyle(DEFAULT, TEXT_SPACING, 1);
+//    Color defTextCLR = GetColor(GuiGetStyle(DEFAULT, LINE_COLOR));
 //
-//    //RGBA
-//    ncnn::destroy_gpu_instance();
-//    return 0;
+//    SetTargetFPS(60);
+//    // Set all game variables.
+//    // Player paddle vars.
+//    int plPosX = 10;
+//    int plPosY = 285;
+//    // Enemy paddle vars.
+//    int enPosX = 1260;
+//    int enPosY = 285;
+//    // Paddle shared vars.
+//    int paSizX = 10;
+//    int paSizY = 150;
+//    // Ball vars.
+//    int baPosX = 22;
+//    int baPosY = 356;
+//    int baVelX = 2;
+//
+//    // Main game loop.
+//    while (!WindowShouldClose()) {
+//        int baVelY = GetRandomValue(-6, 7);
+//        int baSizA = GetRandomValue(8, 25);
+//
+//        // Player paddle movement.
+//        if ((IsKeyDown(KEY_UP)) && (plPosY > 5)) plPosY -= 2;
+//        else if ((IsKeyDown(KEY_DOWN)) && (plPosY + 150 < 715)) plPosY += 2;
+//        // Enemy paddle movement.
+//        if (baPosX > 640) {
+//            if ((enPosY + 70 > baPosY) && (enPosY > 5)) enPosY -= 2;
+//            else if ((enPosY + 80 < baPosY) && (enPosY + 150 < 715)) enPosY += 2; }
+//        // Paddle - Ball collison.
+//        if ((baPosX <= plPosX + 10) && (baPosY >= plPosY) && (baPosY <= plPosY + paSizY)) baVelX = -baVelX;
+//        else if ((baPosX >= enPosX - 10) && (baPosY >= enPosY) && (baPosY <= enPosY + paSizY)) {
+//            baVelX = -baVelX;
+//            baVelY = GetRandomValue(-2, 2); }
+//        // Edge - Ball collsion.
+//        if (baPosY < 5) baVelY = -baVelY;
+//        else if (baPosY + 8 > 715) baVelY = -baVelY;
+//        // Ball movement and Game Over logic.
+//        if ((baPosX > 10) && (baPosX < 1270)) {
+//            baPosX += baVelX;
+//            baPosY += baVelY;
+//        } else { break; }
+//        // Draw to screen.
+//        BeginDrawing();
+//        ClearBackground(DARKGRAY);
+//        // Player paddle
+//        DrawRectangle(plPosX, plPosY, paSizX, paSizY, RAYWHITE);
+//        // Enemy paddle.
+//        DrawRectangle(enPosX, enPosY, paSizX, paSizY, DARKGREEN);
+//        // Ball.
+//        DrawRectangle(baPosX, baPosY, baSizA, baSizA, RED);
+//        EndDrawing();
+//    }
 //}
